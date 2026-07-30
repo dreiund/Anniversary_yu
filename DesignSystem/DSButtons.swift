f9c403ccad67107cedef3f4fd4bdf6f29017cdc8
fkc <lukaskecheng@gmail.com>
2026-07-30 14:11:07 +0800

    修正按钮预览文案以符合六字上限

--- DesignSystem/DSButtons.swift
+++ DesignSystem/DSButtons.swift
@@ -58,7 +58,7 @@ struct DarkUtilityButtonStyle: ButtonStyle {
     VStack(spacing: 16) {
         Button("添加日程") {}.buttonStyle(BluePillButtonStyle())
         Button("封盘") {}.buttonStyle(BluePillButtonStyle(fullWidth: true))
-        Button("接受 TA 的邀请") {}.buttonStyle(GhostPillButtonStyle())
+        Button("接受邀请") {}.buttonStyle(GhostPillButtonStyle())
         Button("编辑") {}.buttonStyle(DarkUtilityButtonStyle())
     }
     .padding()
